8a401ccb0306f5833a77316807dfc33ac0c81ec8
LizaRoma-lab <Elizaveta.Romanova.m@yandex.ru>
2024-08-27 09:43:44 +0300

    commit 03

--- ls-java-core/src/main/java/ru/ls/qa/school/core/Main.java
+++ ls-java-core/src/main/java/ru/ls/qa/school/core/Main.java
@@ -2,6 +2,12 @@ package ru.ls.qa.school.core;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        someMethod();
+    }
+
+    private static void someMethod() {
+        System.out.println("t-3 is dune");
     }
 }
